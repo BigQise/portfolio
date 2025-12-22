f53495efec2e6418fb064b6951188e1197e3a62e
SoroBoio <qise360@gmail.com>
2025-12-21 21:53:42 -0600

    Updated bio section padding

--- app/components/Bio.tsx
+++ app/components/Bio.tsx
@@ -7,7 +7,7 @@ const words = `I am a junior CS Student located in Minnesota, USA. I'm most fami
 
 export default function Bio() {
     return (
-        <div id="bio" className="dark:bg-slate-950 h-150 flex items-center justify-center p-20">
+        <div id="bio" className="dark:bg-slate-950 flex items-center justify-center py-20 lg:py-60">
             <TextGenerateEffect words={words} className="text-foreground text-3xl lg:text-5xl font-bold m-15"/>
         </div>
     );
